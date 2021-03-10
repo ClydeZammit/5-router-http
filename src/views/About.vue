df3774e305ffa8b95f54ed8a4a07ca610e5edf4d
czammit001 <clyde.zammit.c10334@mcast.edu.mt>
2021-03-10 16:42:57 +0100

    Changes in About

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <button type="button" class="btn btn-danger">Button 1</button>
+    <button type="button" class="btn btn-danger">Button 2</button>
   </div>
 </template>
