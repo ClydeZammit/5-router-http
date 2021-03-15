0c743a047c33b27c486218df5fd57b523621ede0
czammit001 <clyde.zammit.c10334@mcast.edu.mt>
2021-03-15 12:09:02 +0100

    Modified About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <button type="button" class="btn btn-danger">Button 1</button>
-    <button type="button" class="btn btn-danger">Button 2</button>
-    <button type="button" class="btn btn-danger">Button 1</button>
-    <button type="button" class="btn btn-danger">Button 2</button>
+    <button type="button" class="btn btn-danger">Click Me!</button>
   </div>
 </template>
